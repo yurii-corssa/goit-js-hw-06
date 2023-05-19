e480c88f8592dfd50348f53f5743b8db68316440
yurii-corssa <yurii.corssa@gmail.com>
2023-05-19 23:40:37 +0200

    Create task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,29 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const makeGalleryEl = (imagesArray) => {
+  const makeGalleryMarkup = ({ url, alt }) =>
+    `<li class="gallery-list">
+        <img class="gallery-img" src="${url}" alt="${alt}" />
+    </li>`;
+
+  const galleryListEl = document.querySelector(".gallery");
+
+  const makeGalleryList = imagesArray.map((image) => makeGalleryMarkup(image));
+
+  galleryListEl.insertAdjacentHTML("afterbegin", makeGalleryList.join(""));
+};
+
+makeGalleryEl(images);
